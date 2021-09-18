6d86245a5b6a14a675f926de28b166f6a7cc2c23
lucadentella <luca@dentella.it>
2021-09-18 11:45:21 +0200

    Update cameraClient.py

--- cameraClient/cameraClient.py
+++ cameraClient/cameraClient.py
@@ -1,5 +1,6 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+from RPi import GPIO
 import time
 import sys
 import cv2
@@ -7,44 +8,33 @@ import zbarlight
 import requests
 from PIL import Image
 
-# Debug mode
-DEBUG = False
-if len(sys.argv) > 1:
-	DEBUG = sys.argv[-1] == 'DEBUG'
-
-# Configuration options
-FULLSCREEN = not DEBUG
-if not DEBUG:
-    RESOLUTION = (640, 480)
-else:
-	RESOLUTION = (480, 270)
+# Initialise GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
+GPIO.output(2, GPIO.LOW)
+GPIO.output(3, GPIO.LOW)
+print("GPIO configured");
 
 # Initialise Raspberry Pi camera
 camera = PiCamera()
 camera.resolution = (640, 480)
-#camera.framerate = 10
-camera.vflip = True
-#camera.hflip = True
-#camera.color_effects = (128, 128)
 
 # set up stream buffer
 rawCapture = PiRGBArray(camera, size=RESOLUTION)
 
 # allow camera to warm up
 time.sleep(0.1)
-
-# Initialise OpenCV window
-#if FULLSCREEN:
-#	cv2.namedWindow("#iothack15", cv2.WND_PROP_FULLSCREEN)
-#	cv2.setWindowProperty("#iothack15", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-#else:
-cv2.namedWindow("#iothack15")
-
 print("cameraClient ready")
 
+# Initialise OpenCV window
+cv2.namedWindow("raspberry-dgc")
+print("OpenCV window ready")
+
 # Capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # as raw NumPy array
+for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = True):
+    
+	# as raw NumPy array
     output = frame.array.copy()
 
     # raw detection code
@@ -55,18 +45,32 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     # create a reader
     codes = zbarlight.scan_codes(['qrcode'], pil)
-    if codes is not None:
+    
+	# if a qrcode was found, call validatorServer
+	if codes is not None:
 
         payload = {'dgc': codes[0]}
         print(payload)
         r = requests.get('http://localhost:3000/', params=payload)
         print('Return code: ', r.status_code, ', Text: ', r.text)
+		
+		# turn on the LEDs for 2 seconds
+		if r.status_code == 200: pin = 2
+        else: pin = 3
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(pin, GPIO.LOW)
 
     # show the frame
-    cv2.imshow("#iothack15", output)
+    cv2.imshow("raspberry-dgc", output)
 
     # clear stream for next frame
     rawCapture.truncate(0)
+	
+	# Wait for Q to quit
+    keypress = cv2.waitKey(1) & 0xFF
+    if keypress == ord('q'):
+        break
 
 # When everything is done, release the capture
 camera.close()
