34dbfa2d65683a63db049b76a75f7e8abb3f87c7
lucadentella <luca@dentella.it>
2021-09-24 08:55:41 +0200

    Molecular tests added - closes #4

--- validatorServer/test.js
+++ validatorServer/test.js
@@ -6,13 +6,20 @@ dayjs.extend(timezone)
 
 const RAPID_TEST_START_HOUR = "rapid_test_start_hours";
 const RAPID_TEST_END_HOUR = "rapid_test_end_hours";
+const MOLECULAR_TEST_START_HOUR = "molecular_test_start_hours";
+const MOLECULAR_TEST_END_HOUR = "molecular_test_end_hours";
 
 const DETECTED = "260373001";
 const NOT_DETECTED = "260415000";
 
+// https://ec.europa.eu/health/sites/default/files/ehealth/docs/digital-green-certificates_dt-specifications_en.pdf
+const TYPE_RAPID = "LP217198-3"; 	// RAT, Rapid Antigen Test
+const TYPE_MOLECULAR = "LP6464-4";	// NAAT, Nucleic Acid Amplification Test
+
 const validateTest = function(settings, dcc) {
 	
     var obj = dcc.payload.t[dcc.payload.t.length -1];
+	var testType = obj.tt;
     var testResult = obj.tr;
 	var dateTimeOfSampleCollection = obj.sc;
     var now = dayjs();
@@ -22,11 +29,26 @@ const validateTest = function(settings, dcc) {
 	
 	try {
 		
-		// 
 		var odtDateTimeOfCollection = dayjs.tz(dateTimeOfSampleCollection, "UTC");
 		var ldtDateTimeOfCollection = odtDateTimeOfCollection.tz(dayjs.tz.guess());
-		var startDate = ldtDateTimeOfCollection.add(parseInt(getRapidTestStartHour(settings).value), 'hours')
-		var endDate = ldtDateTimeOfCollection.add(parseInt(getRapidTestEndHour(settings).value), 'hours')
+		
+		var startDate;
+		var endDate;
+		
+		// Rapid Test
+		if(testType == TYPE_RAPID) {
+			startDate = ldtDateTimeOfCollection.add(parseInt(getRapidTestStartHour(settings).value), 'hours')
+			endDate = ldtDateTimeOfCollection.add(parseInt(getRapidTestEndHour(settings).value), 'hours')
+		}
+		
+		// Molecular Test
+		else if(testType == TYPE_MOLECULAR) {
+			startDate = ldtDateTimeOfCollection.add(parseInt(getMolecularTestStartHour(settings).value), 'hours')
+			endDate = ldtDateTimeOfCollection.add(parseInt(getMolecularTestEndHour(settings).value), 'hours')
+		}
+		
+		// Unknown Test
+		else return {result: false, message: "Unknown test type"};
 		
 		if(startDate.isAfter(now)) return {result: false, message: "Not valid yet"};
 		if(now.isAfter(endDate)) return {result: false, message: "Expired"};
@@ -50,6 +72,20 @@ function getRapidTestEndHour(settings) {
 	
     return settings.find(it => {
         return it.name == RAPID_TEST_END_HOUR
+    })
+}
+
+function getMolecularTestStartHour(settings) {
+	
+    return settings.find(it => {
+        return it.name == MOLECULAR_TEST_START_HOUR
+    })
+
+}
+function getMolecularTestEndHour(settings) {
+	
+    return settings.find(it => {
+        return it.name == MOLECULAR_TEST_END_HOUR
     })
 }
 
